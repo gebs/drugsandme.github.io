b0156779d2002edcb1c7a96b151ec3087a5c2e92
Gabriel Hirschbaeck <Gab@dhcp-213-144-153-158.init7.net>
2015-02-02 01:37:30 +0100

    added share buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 
+
+
+
+//facebook share
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
 //makes whatever is brought into function, the title, sliding icon to the right, and making a title
 $.fn.entitle = function(){
      var current = $(this).siblings('.item-name').text();
